b02bbfca4e806eeed50dd0854aa794ce64c23746
Kareem Mohamed <kareem3d.a@gmail.com>
2014-02-18 16:28:14 +0200

    Mobile design progress

--- app/views/templates/angular.blade.php
+++ app/views/templates/angular.blade.php
@@ -2,7 +2,7 @@
 <html xmlns:ng="http://angularjs.org" id="ng-app" ng-app="qbrando">
 <head>
     <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 
     <title>Qbrando | Online shop for luxury in Qatar</title>
     <link href='http://fonts.googleapis.com/css?family=Frijole' rel='stylesheet' type='text/css'>
