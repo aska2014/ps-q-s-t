d047292ba3c5b4cf18043a4b32e669e4954118ea
Kareem Mohamed <kareem3d.a@gmail.com>
2014-02-19 14:08:14 +0200

    Better way to manage mass offers

--- app/freak/views/mass_offer/form.php
+++ app/freak/views/mass_offer/form.php
@@ -1,7 +1,5 @@
 <div class="row-fluid" ng-controller="FEMOCtrl">
     <div class="btn-toolbar">
-        <button ng-click="mass_offer.offer = 'gift'" class="btn btn-large btn-warning">Gift offer</button>
-        <button ng-click="mass_offer.offer = 'discount'" class="btn btn-large btn-danger">Discount offer</button>
     </div>
     <div class="span12">
         <div class="widget">
@@ -9,7 +7,7 @@
                 <span class="title">Add new mass offer</span>
             </div>
             <div class="widget-content form-container">
-                <form class="form-horizontal" ng-submit="processGiftForm()" ng-show="mass_offer.offer == 'gift'">
+                <form class="form-horizontal" ng-submit="processMyForm()">
                     <div class="control-group">
                         <label class="control-label" for="input00">Title</label>
                         <div class="controls">
@@ -35,55 +33,32 @@
                         </div>
                     </div>
                     <div class="control-group">
+                        <label class="control-label" for="input00">Choose offer type</label>
+                        <div class="controls">
+                            <label class="radio">
+                                <input type="radio" ng-model="mass_offer.offer" value="gift">
+                                Gift offer
+                            </label>
+                            <label class="radio">
+                                <input type="radio" ng-model="mass_offer.offer" value="discount">
+                                Discount offer
+                            </label>
+                        </div>
+                    </div>
+                    <div class="control-group" ng-show="mass_offer.offer == 'gift'">
                         <label class="control-label" for="input00">Gifts per one product</label>
                         <div class="controls">
                             <input type="text" id="input00" class="span12" ng-model="model.gifts_per_product">
+                            <small>For each <strong>{{ 1/model.gifts_per_product }}</strong> products you get one gift</small>
                         </div>
                     </div>
-                    <div class="control-group">
+                    <div class="control-group" ng-show="mass_offer.offer == 'gift'">
                         <label class="control-label" for="input00">Max gift price</label>
                         <div class="controls">
                             <input type="text" id="input00" class="span12" ng-model="model.max_gift_price">
                         </div>
                     </div>
-                    <div class="control-group">
-                        <label class="control-label" for="input00">Date range</label>
-                        <div class="controls">
-                            <input fr-datetime-picker type="text" class="span3" ng-model="model.from_date" placeholder="from date">
-                            <input fr-datetime-picker type="text" class="span3" ng-model="model.to_date" placeholder="to date">
-                        </div>
-                    </div>
-                    <div class="form-actions">
-                        <button type="submit" class="btn btn-primary">Save changes</button>
-                        <button type="reset" class="btn" type="reset">Cancel</button>
-                    </div>
-                </form>
-                <form class="form-horizontal" ng-submit="processDiscountForm()" ng-show="mass_offer.offer == 'discount'">
-                    <div class="control-group">
-                        <label class="control-label" for="input00">Title</label>
-                        <div class="controls">
-                            <input type="text" id="input00" class="span12" ng-model="model.title">
-                        </div>
-                    </div>
-                    <div class="control-group">
-                        <label class="control-label" for="input00">Description</label>
-                        <div class="controls">
-                            <input type="text" id="input00" class="span12" ng-model="model.description">
-                        </div>
-                    </div>
-                    <div class="control-group">
-                        <label class="control-label" for="input00">Start quantity</label>
-                        <div class="controls">
-                            <input type="text" id="input00" class="span12" ng-model="model.start_quantity">
-                        </div>
-                    </div>
-                    <div class="control-group">
-                        <label class="control-label" for="input00">Start price</label>
-                        <div class="controls">
-                            <input type="text" id="input00" class="span12" ng-model="model.start_price">
-                        </div>
-                    </div>
-                    <div class="control-group">
+                    <div class="control-group" ng-show="mass_offer.offer == 'discount'">
                         <label class="control-label" for="input00">Discount percentage</label>
                         <div class="controls">
                             <input type="text" id="input00" class="span12" ng-model="model.discount_percentage">
@@ -110,19 +85,26 @@
 <script type="text/javascript">
     function FEMOCtrl($scope)
     {
-        $scope.mass_offer = {offer : 'gift'};
+        $scope.$watch('model.discount_percentage', function(discount) {
 
-        $scope.processGiftForm = function()
+            if(discount !== undefined)
+            {
+                if(discount > 0) $scope.mass_offer = {offer : 'discount'};
+                else $scope.mass_offer = {offer : 'gift'};
+            }
+        });
+
+        $scope.processMyForm = function()
         {
-            $scope.model.discount_percentage = 0;
-
-            $scope.processForm();
-        }
-
-        $scope.processDiscountForm = function()
-        {
-            $scope.model.max_gift_price = 0;
-            $scope.model.gifts_per_product = 0;
+            if($scope.mass_offer.offer == 'discount')
+            {
+                $scope.model.max_gift_price = 0;
+                $scope.model.gifts_per_product = 0;
+            }
+            else
+            {
+                $scope.model.discount_percentage = 0;
+            }
 
             $scope.processForm();
         }
