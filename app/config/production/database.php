d2d691a3e295e793875c7109d905d112a4fb88b2
Kareem Mohamed <kareem3d.a@gmail.com>
2014-03-12 14:46:24 +0200

    Test request

--- app/config/production/database.php
+++ app/config/production/database.php
@@ -15,5 +15,17 @@ return array(
             'prefix'    => 'krq_',
         ),
 
+        'mysql' => array(
+            'driver'    => 'mysql',
+            'host'      => 'localhost',
+            'database'  => 'speedupb_akmo',
+            'username'  => 'speedupb_kemo',
+            'password'  => '*&ADNSKYH$#A',
+            'charset'   => 'utf8',
+            'collation' => 'utf8_unicode_ci',
+            'prefix'    => 'krq_',
+        ),
+
+
     )
 );
